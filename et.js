f1ce572aa63ccf3318bbf39fef306243d4d754fe
Cwaddick <120151176+Cwaddick@users.noreply.github.com>
2024-02-25 23:37:31 -0500

    Update et.js

--- et.js
+++ et.js
@@ -1,1 +1,27 @@
-
+const eternalsCharacters = [
+  {
+    name: "Ikaris",
+    attributes: ["flight", "super strength", "laser sight", "unarmed combat", "enhanced senses"],
+    
+  },
+  {
+    name: "Sersi",
+    attributes: ["matter manipulation", "telepathy", "stage 1 immortality","transmuatation","enhanced physique"],
+    
+  },
+  {
+    name: "Thena",
+    attributes: ["combat proficiency", "regeneration", "meta-physique","Weapon generation","Psionic resistance"],
+    
+  },
+  {
+    name: "Phastos",
+    attributes: ["healing factor", "matter manipulation", "cosmic energy manipulation","technomancy","enhanced durability"],
+    
+  },
+  {
+    name: "Druig",
+    attributes: ["Telepathy", "mind control", "possession","Longevity", "immortality"],
+    
+  }
+];
